4c7a41ba16d5fe783ae375626e232da44b0559f0
Nyde2283 <edwynchev@gmail.com>
2022-07-12 22:45:41 +0200

    Fix indentation

--- main.py
+++ main.py
@@ -6,23 +6,23 @@ from rich import box
 
 
 def affichage(t, function, derivee):
-     assert type(t)==list, f't doit être une liste (type(t) = {type(t)})'
-     assert len(t)>=2, f'Le tableau est trop petit (nb_lignes = {len(t)} < 2)'
-     assert len(t[0])>=5, f'Le tableau est trop petit (nb_colonnes = {len(t[0])} < 4)'
-     for i in range(len(t)):
-          assert len(t[i])==len(t[0]), f'Les lignes du tableau n\'ont pas la même longueur (len t[0]= {len(t[0])}, len t[{i}]= {len(t[i])}'
+    assert type(t)==list, f't doit être une liste (type(t) = {type(t)})'
+    assert len(t)>=2, f'Le tableau est trop petit (nb_lignes = {len(t)} < 2)'
+    assert len(t[0])>=5, f'Le tableau est trop petit (nb_colonnes = {len(t[0])} < 4)'
+    for i in range(len(t)):
+        assert len(t[i])==len(t[0]), f'Les lignes du tableau n\'ont pas la même longueur (len t[0]= {len(t[0])}, len t[{i}]= {len(t[i])}'
 
-     table = Table(box=box.SIMPLE, padding=(0,2,0,2), leading=1)
-     for i in range(len(t[0])):
-          table.add_column(t[0][i], justify='center') #ajout des en-têtes de colonnes un par un (première ligne de t)
-     for i in range(1, len(t)):
-          table.add_row(*t[i]) #ajout des lignes une par une
+    table = Table(box=box.SIMPLE, padding=(0,2,0,2), leading=1)
+    for i in range(len(t[0])):
+        table.add_column(t[0][i], justify='center') #ajout des en-têtes de colonnes un par un (première ligne de t)
+    for i in range(1, len(t)):
+        table.add_row(*t[i]) #ajout des lignes une par une
 
-     console = Console()
-     console.print(table) # affichage de la table
-     print()
-     print(function, derivee, sep='   |   ')
-     print()
+    console = Console()
+    console.print(table) # affichage de la table
+    print()
+    print(function, derivee, sep='   |   ')
+    print()
 
 
 def generate_table_affine(zero, signe, varia):
