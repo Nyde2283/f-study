72cf103fed36db7bbbf4a521bdd7051aca3154d5
Nyde2283 <edwynchev@gmail.com>
2022-07-12 14:47:47 +0200

    Fix assertion error
    Le nombre minimal de colonnes est passé de 4 à 5.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from rich import box
 def affichage(t):
      assert type(t)==list, f't doit être une liste (type(t) = {type(t)})'
      assert len(t)>=2, f'Le tableau est trop petit (nb_lignes = {len(t)} < 2)'
-     assert len(t[0])>=4, f'Le tableau est trop petit (nb_colonnes = {len(t[0])} < 4)'
+     assert len(t[0])>=5, f'Le tableau est trop petit (nb_colonnes = {len(t[0])} < 4)'
      for i in range(len(t)):
           assert len(t[i])==len(t[0]), f'Les lignes du tableau n\'ont pas la même longueur (len t[0]= {len(t[0])}, len t[{i}]= {len(t[i])}'
 
