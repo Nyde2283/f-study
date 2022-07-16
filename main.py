1647ca11a2f66d460dec5583a8cd5f9b93107adb
Nyde2283 <edwynchev@gmail.com>
2022-07-16 15:34:11 +0200

    Change syntaxe

--- main.py
+++ main.py
@@ -27,7 +27,6 @@ def selection():
     elif select == '2': selection()
     else: selection()
 
-
 def selection_affine():
     print()
     print('f(x) = [i green blink]a[/i green blink]x+[i green]b[/i green]\n')
@@ -41,28 +40,6 @@ def selection_affine():
     if b.is_integer(): b = int(b)
 
     anal_affine(a, b)
-
-def affichage(t, function, derivee):
-    assert type(t)==list, f't doit être une liste (type(t) = {type(t)})'
-    assert len(t)>=2, f'Le tableau est trop petit (nb_lignes = {len(t)} < 2)'
-    assert len(t[0])>=5, f'Le tableau est trop petit (nb_colonnes = {len(t[0])} < 4)'
-    for i in range(len(t)):
-        assert len(t[i])==len(t[0]), f'Les lignes du tableau n\'ont pas la même longueur (len t[0]= {len(t[0])}, len t[{i}]= {len(t[i])}'
-
-    table = Table(box=box.SIMPLE, padding=(0,2,0,2), leading=1)
-    for i in range(len(t[0])):
-        table.add_column(t[0][i], justify='center') #ajout des en-têtes de colonnes un par un (première ligne de t)
-    for i in range(1, len(t)):
-        table.add_row(*t[i]) #ajout des lignes une par une
-
-    system('cls')
-    print('\n')
-    print(table) # affichage de la table
-    print()
-    print(function, derivee, sep='\n')
-    print('\n')
-    print('[#63666A]Certaines valeurs peuvent être arrondies.[/#63666A]')
-    input('\nAppuyer sur Enter pour quitter...')
 
 
 def generate_table_affine(zero, signe, varia):
@@ -84,20 +61,14 @@ def anal_affine(a, b):
 
     function = 'f(x) = '
     if a==0:
-        if b==0:
-            function += '0'
+        if b==0: function += '0' #0x+0 = x
     else:
-        if a==1:
-            function += 'x'
-        elif a==-1:
-            function += '-x'
-        else:
-            function += f'{a}x'
-        if b>0:
-            function += f'+{b}'
-        else:
-            function += f'{b}'
-    
+        if a==1: function += 'x' #1x = x
+        elif a==-1: function += '-x' #-1x = -x
+        else: function += f'{a}x'
+        if b>0: function += f'+{b}' #pour éviter d'afficher 5x+-8 (à la place de 5x-8) par exemple
+        else: function += f'{b}'
+
     derivee = f'[#B4009E]f\'[/#B4009E](x) = {a}'
 
     if a>0:
@@ -123,6 +94,29 @@ def anal_affine(a, b):
     
     table = generate_table_affine(zero, signe, variation)
     affichage(table, function, derivee)
+
+
+def affichage(t, function, derivee):
+    assert type(t)==list, f't doit être une liste (type(t) = {type(t)})'
+    assert len(t)>=2, f'Le tableau est trop petit (nb_lignes = {len(t)} < 2)'
+    assert len(t[0])>=5, f'Le tableau est trop petit (nb_colonnes = {len(t[0])} < 4)'
+    for i in range(len(t)):
+        assert len(t[i])==len(t[0]), f'Les lignes du tableau n\'ont pas la même longueur (len t[0]= {len(t[0])}, len t[{i}]= {len(t[i])}'
+
+    table = Table(box=box.SIMPLE, padding=(0,2,0,2), leading=1)
+    for i in range(len(t[0])):
+        table.add_column(t[0][i], justify='center') #ajout des en-têtes de colonnes un par un (première ligne de t)
+    for i in range(1, len(t)):
+        table.add_row(*t[i]) #ajout des lignes une par une
+
+    system('cls')
+    print('\n')
+    print(table) # affichage de la table
+    print()
+    print(function, derivee, sep='\n')
+    print('\n')
+    print('[#63666A]Certaines valeurs peuvent être arrondies.[/#63666A]')
+    input('\nAppuyer sur Enter pour quitter...')
 
 
 
