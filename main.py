c1eb2675f837ad3521b8d00d27d60cbbd30ba3ed
Nyde2283 <edwynchev@gmail.com>
2022-08-13 00:49:31 +0200

    Ajout affichage 2nd degré

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ def generate_table_affine(zero, signe, varia):
         t = [['', 'x=', '-∞', '', '+∞'],
              ['signe', 'f(x)', '', signe[0], ''],
              ['varia', 'f(x)', '', varia, '']]
-    return t
+    return [t]
 
 def anal_affine(a, b):
     '''Analyse une fonction de la forme f(x) = ax+b'''
@@ -113,8 +113,8 @@ def anal_affine(a, b):
         signe = ('0')
         variation = '→'
     
-    table = generate_table_affine(zero, signe, variation)
-    affichage(table, function, derivee)
+    tableau = generate_table_affine(zero, signe, variation)
+    affichage(tableau, function, derivee)
 
 
 def anal_2nd_degre(a, b, c):
@@ -187,22 +187,16 @@ def generate_table_2nd_degre(a, racines, S):
 
     return [t_signe, t_varia]
 
-def affichage(t, function, derivee):
-    assert type(t)==list, f't doit être une liste (type(t) = {type(t)})'
-    assert len(t)>=2, f'Le tableau est trop petit (nb_lignes = {len(t)} < 2)'
-    assert len(t[0])>=5, f'Le tableau est trop petit (nb_colonnes = {len(t[0])} < 4)'
-    for i in range(len(t)):
-        assert len(t[i])==len(t[0]), f'Les lignes du tableau n\'ont pas la même longueur (len t[0]= {len(t[0])}, len t[{i}]= {len(t[i])}'
-
-    table = Table(box=box.SIMPLE, padding=(0,2,0,2), leading=1)
-    for i in range(len(t[0])):
-        table.add_column(t[0][i], justify='center') #ajout des en-têtes de colonnes un par un (première ligne de t)
-    for i in range(1, len(t)):
-        table.add_row(*t[i]) #ajout des lignes une par une
-
+def affichage(tableaux, function, derivee):
     system('cls')
-    print('\n')
-    print(table) # affichage de la table
+    for t in tableaux:
+        table = Table(box=box.SIMPLE, padding=(0,2,0,2), leading=1)
+        for i in range(len(t[0])):
+            table.add_column(t[0][i], justify='center') #ajout des en-têtes de colonnes un par un (première ligne de t)
+        for i in range(1, len(t)):
+            table.add_row(*t[i]) #ajout des lignes une par une
+        print('\n')
+        print(table) # affichage de la table
     print()
     print(function, derivee, sep='\n')
     print('\n')
