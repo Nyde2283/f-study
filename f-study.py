e604676397ee2d5a06236a670d23101a4a5159d8
Nyde2283 <edwynchev@gmail.com>
2022-08-29 00:51:45 +0200

    Add display method

--- f-study.py
+++ f-study.py
@@ -22,8 +22,64 @@ console = Console(highlight=False)
 
 
 class Fonction:
-    def __init__(self) -> None:
-        pass 
+    def __init__(self, name):
+        self.name = name
+
+    def display(self):
+        t_signe = [
+            ['', 'x='],
+            ['signe', f'{self.name}(x)']
+        ]
+        t_varia = [
+            ['', 'x='],
+            ['signe', f'{self.name}\'(x)'],
+            ['varia', f'{self.name}(x)']
+        ]
+        for i in range(len(self.f_signe)):
+            if i%2==0:
+                if self.f_signe[i] in ('-∞', '+∞'):
+                    t_signe[0].append(self.f_signe[i])
+                    t_signe[1].append('')
+                else:
+                    t_signe[0].append(self.f_signe[i])
+                    t_signe[1].append('0')
+            elif i%2==1:
+                t_signe[0].append('')
+                t_signe[1].append(self.f_signe[i])
+        for i in range(len(self.f_varia)):
+            if i%2==0:
+                if self.f_varia[i] in ('-∞', '+∞'):
+                    t_varia[0].append(self.f_varia[i])
+                    t_varia[1].append('')
+                    t_varia[2].append('')
+                else:
+                    t_varia[0].append(self.f_varia[i]["x"])
+                    t_varia[1].append('0')
+                    t_varia[2].append(self.f_varia[i]["y"])
+            else:
+                match self.f_varia[i]:
+                    case '+':
+                        varia = ('+', '↗')
+                    case '-':
+                        varia = ('-', '↘')
+                    case '0':
+                        varia = ('0', '→')
+                t_varia[0].append('')
+                t_varia[1].append(varia[0])
+                t_varia[2].append(varia[1])
+
+        for t in (t_signe, t_varia):
+            tableau = Table(box=box.SIMPLE, padding=(0,2,0,2), leading=1)
+            for i in range(len(t[0])):
+                tableau.add_column(t[0][i], justify='center') #ajout des en-têtes de colonnes un par un (première ligne de t)
+            for i in range(1, len(t)):
+                tableau.add_row(*t[i]) #ajout des lignes une par une
+            console.print(tableau) # affichage de la table
+        console.print('\n')
+        console.print(self.fonction, self.derivee, sep='\n')
+        console.print('\n')
+        console.print('[#818488]Certaines valeurs peuvent être arrondies.[/#818488]')
+
 
 
 
