56211adedf3c69bd42bdd76d803e831cfbb41d3c
Nyde2283 <edwynchev@gmail.com>
2022-08-29 00:49:17 +0200

    Add import

--- f-study.py
+++ f-study.py
@@ -1,3 +1,26 @@
+from sys import version_info
+assert version_info >= (3, 10), 'f-study nécessite Python 3.10 ou supérieur'
+
+try:
+    from rich.console import Console
+except:
+    print('\nLa bibliothèque Rich n\'est pas installé sur votre ordinateur. Elle est indispensable pour afficher les tableaux.')
+    print('Pour plus d\'informations consultez la page PyPi ( https://pypi.org/project/rich/ ) \nou la page GitHub ( https://github.com/Textualize/rich ) du projet.')
+    print('\nPour installer Rich :')
+    print('- Ouvrez un terminal en administrateur')
+    print('- Tapez la commande: python -m pip install rich')
+    input('\n Appuyez sur Enter pour quitter...')
+from rich.table import Table
+from rich import box
+from math import sqrt
+from os import system
+
+
+console = Console(highlight=False)
+
+
+
+
 class Fonction:
     def __init__(self) -> None:
         pass 
