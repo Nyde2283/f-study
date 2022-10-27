d00afdf506bcbec1c8b94483dca7750061cc6748
Nyde2283 <edwynchev@gmail.com>
2022-10-27 23:21:33 +0200

    Fix check_args pour les méthodes

--- decorators.py
+++ decorators.py
@@ -1,46 +1,35 @@
-import functools
+from functools import wraps
 
 
-def check_args(fct) -> None:
+def check_args(fct):
     """Test les paramètres de la fonction décorée
 
     Args:
         fct (function): fonction décorée
-
-    Returns:
-        None
     """
-    @functools.wraps(fct)
+    args_names = fct.__code__.co_varnames
+    @wraps(fct)
     def check(*args, **kwargs):
         args_repr = [repr(a) for a in args]
         kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]
         appel = f'{fct.__name__}({", ".join(args_repr + kwargs_repr)})'
-
-        if fct.__name__=='__init__': array = len(args)-1
-        else: array = len(args)
-        for i in range(array):
-            if fct.__name__=='__init__': j=i+1
-            else: j=i
-            annot_key_value = list(fct.__annotations__.items())[i]
-            annot_value = list(fct.__annotations__.values())[i]
-
-            if type(annot_value) == type:
-                if type(args[j]) != annot_value:
-                    raise TypeError(f'{repr(annot_key_value)} attendu | appel: {appel}')
-            else:
-                if args[j] != annot_value:
-                    raise ValueError(f'{repr(annot_key_value)} attendu | appel: {appel}')
-
-        for arg, value in kwargs.items():
-            annot_key_value = [item for item in list(fct.__annotations__.items()) if arg in item][0]
-            annot_value = fct.__annotations__[arg]
-
-            if type(annot_value) == type:
-                if type(value) != annot_value:
-                    raise TypeError(f'{repr(annot_key_value)} attendu | appel: {appel}')
-            else:
-                if value != annot_value:
-                    raise ValueError(f'{repr(annot_key_value)} attendu | appel: {appel}')
+        dict_args_value = {}
+        for i in range(len(args)):
+            dict_args_value[args_names[i]] = args[i]
+        for key, value in kwargs.items():
+            dict_args_value[key] = value
+        
+        for arg, value in dict_args_value.items():
+            annot_key_type = [item for item in list(fct.__annotations__.items()) if arg in item] #liste avec un seul élément
+            if len(annot_key_type) != 0: #si l'argument est compris dans les annotations
+                annot_key_type = annot_key_type[0]
+                annot_type = annot_key_type[1]
+                if type(annot_type) == type:
+                    if type(value) != annot_type:
+                        raise TypeError(f'{repr(annot_key_type)} attendu | appel: {appel}')
+                else:
+                    if value != annot_type:
+                        raise ValueError(f'{repr(annot_key_type)} attendu | appel: {appel}')
 
         return fct(*args, **kwargs)
     return check
