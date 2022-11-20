79a1f6d6f8cf0b78c96ebacdcb698f88af9620cc
Nyde2283 <edwynchev@gmail.com>
2022-11-20 17:04:39 +0100

    Add Interval class

--- f-study.py
+++ f-study.py
@@ -1,4 +1,4 @@
-from math import sqrt
+from math import sqrt, inf
 from os import system
 from platform import system as platform
 from sys import version_info
@@ -221,6 +221,43 @@ def anal_second_degre(name: str, facteurs: list) -> tuple[str, str, list[str], l
 
     return fonction, derivee, signe, varia
 
+class Interval:
+    def __init__(self, begin: int | float, end, exclude_values = []) -> None:
+        assert (type(begin) in (int, float)) or begin == -inf
+        assert (type(end) in (int, float)) or begin == inf
+        assert begin <= end
+        
+        self.begin = begin
+        self.end = end
+        self.excludes = []
+        for e in sorted(exclude_values):
+            if (self.begin < e < self.end) and (e not in self.excludes):
+                self.excludes.append(e)
+
+    def __str__(self) -> str:
+        result = f'[{self.begin} '
+        for e in self.excludes:
+            result += f'|{e}| '
+        result += f'-> {self.end}]'
+        return result
+
+    def __contains__(self, other):
+        return self.begin <= other <= self.end and (other not in self.excludes)
+
+    def __add__(self, other):
+        assert type(other) == Interval
+        a = self.begin
+        b = self.end
+        c = other.begin
+        d = other.end
+
+        if (c > b) or (d < a):
+            raise ValueError
+        if a < c: a = c
+        if b > d: b = d
+        excludes = self.excludes + other.excludes
+
+        return Interval(a, b, excludes)
 
 class Fonction:
     @check_args
